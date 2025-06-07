6a52f4cfd33a8a551da8b4dcf1803debb4a6c4b9
carraracaio <caio.carrara20@gmail.com>
2025-06-07 01:33:41 -0300

    p

--- app/catalogo/page.tsx
+++ app/catalogo/page.tsx
@@ -2,7 +2,63 @@ export default function Catalogo() {
     return (
     <>
     
-    <h1>Olá, esta é a pagina catalogo</h1>;
+    <h1>Discos variados e com belos preços</h1>
+
+
+    <table style={{ borderCollapse: "collapse",
+                     width: "100%" 
+                    
+                    }} 
+    cellSpacing="0" 
+    cellPadding="8">
+      <thead style={{ backgroundColor: "#f2f2f2" }}>
+        <tr>
+          <th>Nome</th>
+          <th>Autor</th>
+          <th>Estilo</th>
+          <th>Ano</th>
+          <th>Preço</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Thriller</td>
+          <td>Michael Jackson</td>
+          <td>Pop</td>
+          <td>1982</td>
+          <td>R$ 75,00</td>
+        </tr>
+        <tr>
+          <td>Back in Black</td>
+          <td>AC/DC</td>
+          <td>Rock</td>
+          <td>1980</td>
+          <td>R$ 60,00</td>
+        </tr>
+        <tr>
+          <td>Kind of Blue</td>
+          <td>Miles Davis</td>
+          <td>Jazz</td>
+          <td>1959</td>
+          <td>R$ 90,00</td>
+        </tr>
+        <tr>
+          <td>The Dark Side of the Moon</td>
+          <td>Pink Floyd</td>
+          <td>Progressive Rock</td>
+          <td>1973</td>
+          <td>R$ 80,00</td>
+        </tr>
+        <tr>
+          <td>Discovery</td>
+          <td>Daft Punk</td>
+          <td>Eletrônica</td>
+          <td>2001</td>
+          <td>R$ 70,00</td>
+        </tr>
+      </tbody>
+    </table>
+
 
     </>
 );
--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -1,13 +1,19 @@
+import Image from 'next/image';
 export default function Sobre() {
     return (
 <>
-       <h1>Sobre a Deja-Vú</h1>,
-    <p>O conceito de Deja-Vú vem de algo que parece já ter sido visto, <br />
-       entretanto, é algo novo, tal qual como os discos vendidos nesta loja <br/></p>,
-       <img src="@/images/LP showcase.png"/> 
+       <h1 style={{ textAlign: "center" }}>Sobre a Deja-Vú</h1>
 
-
-
+    <p style={{ textAlign: "center" }}>O conceito de Deja-Vú vem de algo que parece já ter sido visto, <br />
+       entretanto, é algo novo, tal qual como os discos vendidos nesta loja <br/></p>
+       
+<div style={{ textAlign: "center" }}>
+       <Image src="/images/LP_showcase.png"
+        alt="Logo da loja Deja-Vú"
+         width={400}
+        height={300}
+         /> 
+</div>
 </>
  );
  }
