4d33c64feea3f59c5876d83f1a72f756b573746c
carraracaio <caio.carrara20@gmail.com>
2025-06-07 00:06:58 -0300

    p

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,17 @@
 
 
 "use client";
-import { Header, Sidebar } from "../components";
-import styles from "@/styles/componentes/layout.module.css"
+import { Header} from "../components";
 import "@/styles/global.css"; 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) 
 {
   return (
     <html lang="pt-br">
-      <body>
-        
+      <body> 
        <Header />
-
-       <div className = {styles.layoutContainer}>
-
-          <Sidebar />
-          <main className = {styles.mainContent}
-          
-          style={{ flex: 1, 
-                   padding: "10px", 
-                   }}>
-            
             {children}
-
-          </main>
-
-        </div>
-
       </body>
-
     </html>
   );
 }
